da89ca45f92056ab8840a69d496a3286dd742986
Fajr Zafar-nu <fajrzafar-nu@Fajrs-MacBook-Air.local>
2023-10-13 14:04:23 +0500

    Second commit

--- test.py
+++ test.py
@@ -14,19 +14,3 @@ def test_getStatus():
      bulb.isOff()
      assert bulb.getstatus()=="Bulb is not glowing"
 
-def test_toggle_bulb_state():
-    # Initially, the bulb should be off
-    assert bulb.getstatus() == "Bulb is not glowing"
-    
-    # Turn on the bulb
-    bulb.ison()
-    assert bulb.getstatus() == "Bulb is glowing"
-    
-    # Toggle the bulb state
-    bulb.toggle()
-    assert bulb.getstatus() == "Bulb is not glowing"  # The state should be off after toggling
-    
-    # Toggle again
-    bulb.toggle()
-    assert bulb.getstatus() == "Bulb is glowing"  # The state should be on after toggling
-
